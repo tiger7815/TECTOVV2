13121da27304bfc54ea75fefc21d630088478c08
Lord <110597375+kd2929@users.noreply.github.com>
2023-10-22 14:24:32 +0530

    Update main.py

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ bot = Client("bot",
              bot_token= "6253605770:AAGPMePXJh03Z5XVZDUPpKA8sHIMY5rqsTE",
              api_id= 24250238,
              api_hash= "cb3f118ce5553dc140127647edcf3720")
-
+             auth_users= "6175650047")
 
 @bot.on_message(filters.command(["start"]))
 async def account_login(bot: Client, m: Message):
